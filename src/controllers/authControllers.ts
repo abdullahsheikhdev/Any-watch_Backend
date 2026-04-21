ea6a0ab9bf66ae3923e85acf84d8471581c5da54
Md Abdullah Sheikh <mbabdullahsk4220@gmail.com>
2026-04-21 12:23:17 +0600

    ok

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -108,8 +108,8 @@ export const login = async (req : Request, res : Response) => {
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+            secure: true,
+            sameSite:'none',
             maxAge: 4 * 60 * 60 * 1000 // 4 hours   
         });
 
