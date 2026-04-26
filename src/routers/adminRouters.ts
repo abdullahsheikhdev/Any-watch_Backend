bd26ab862fe12ae9a0a7957a9a30d849a4ff0799
Md Abdullah Sheikh <mbabdullahsk4220@gmail.com>
2026-04-26 16:43:04 +0600

    done

--- src/routers/adminRouters.ts
+++ src/routers/adminRouters.ts
@@ -1,5 +1,5 @@
 import express  from "express";
-import { adminAddMovie, adminLogin, adminLogout, deleteMovie, moviesList } from "../controllers/adminController.js";
+import { adminAddMovie, adminLogin, adminLogout, deleteMovie, moviesList, updateMovieStatus } from "../controllers/adminController.js";
 import { adminAuthMiddleware } from "../middleware/adminAuth.js";
 
 const adminRouter = express.Router();
@@ -14,5 +14,6 @@ adminRouter.get('/all-movis', adminAuthMiddleware, moviesList)
 
 
 adminRouter.delete('/delete-movie/:id', adminAuthMiddleware, deleteMovie)
+adminRouter.patch('/update-status/:id', adminAuthMiddleware, updateMovieStatus)
 
 export default adminRouter;
